f5cbbed5ab9366ef5b4280466be7f56709b5da42
Maciej Rak <raczek.maciek@gmail.com>
2023-09-26 15:13:19 +0200

    add DataService struct

---
+++ ShoppingList/DataService.swift
@@ -0,0 +1,16 @@
+//
+//  DataService.swift
+//  ShoppingList
+//
+//  Created by Maciej Rak on 26/09/2023.
+//
+
+import Foundation
+
+struct DataService{
+    var items: [Array] = [
+    ["apple", "banana", "orange", "grape", "pineapple"],
+    ["potato", "tomato", "lettuce", "salad", "cucumber"],
+    ["tooth paste", "shampoo", "soap"]
+    ]
+}
