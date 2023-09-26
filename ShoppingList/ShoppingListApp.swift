0d7f66542e5b88b11bb5c59d9be9f5ad3327a6a5
Maciej Rak <raczek.maciek@gmail.com>
2023-09-26 15:06:47 +0200

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Maciej Rak on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
