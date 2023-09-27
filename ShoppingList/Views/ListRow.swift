47cee8557f591fcba1e3e47d82aad1ead851367a
Maciej Rak <raczek.maciek@gmail.com>
2023-09-27 09:21:57 +0200

    Add counter control buttons

--- ShoppingList/Views/ListRow.swift
+++ ShoppingList/Views/ListRow.swift
@@ -16,7 +16,35 @@ struct ListRow: View {
         HStack{
             Text(itemName)
             Spacer()
-            Text(String(itemCount))
+            LazyVGrid(columns: [
+                GridItem(spacing: 0),
+                GridItem(spacing: 0),
+                GridItem(spacing: 0)]){
+                    Button{
+                        
+                    }label:{
+                        Image(systemName: "minus")
+                            .scaleEffect(0.8)
+                            .padding(.all, 3)
+                            .tint(.primary).colorInvert()
+                    }
+                    .background(Circle()
+                        .frame(height: 20)
+                        .foregroundStyle(.red))
+                    Text(String(itemCount))
+                    Button{
+                        
+                    }label:{
+                        Image(systemName: "plus")
+                            .scaleEffect(0.8)
+                            .padding(.all, 3)
+                            .tint(.primary).colorInvert()
+                    }
+                    .background(Circle()
+                        .frame(height: 20)
+                        .foregroundStyle(.green))
+
+                }
         }
     }
 }
